7f40b634c843a0db1e5a821bc49cc083723d3f1d
NhatMinh666 <nhatminh291297@gmail.com>
2024-06-12 17:33:57 +0700

    Update CreateUserPage.tsx
    Save button now only be enable when all fields are filled.
    Handlers for values change and onclick added.

--- src/pages/user/userCreate/CreateUserPage.tsx
+++ src/pages/user/userCreate/CreateUserPage.tsx
@@ -1,67 +1,129 @@
 import { FC } from "react";
-import { Helmet } from "react-helmet-async";
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './CreateUserForm.css';
 
 const CreateUserPage: FC = () => {
-
+    const [firstName, setFirstName] = useState<string>('');
+    const [lastName, setLastName] = useState<string>('');
+    const [dob, setDob] = useState<string>('');
     const [gender, setGender] = useState<string>('Female');
+    const [joinedDate, setJoinedDate] = useState<string>('');
+    const [type, setType] = useState<string>('Staff');
+    /*const [selectedDate, setSelectedDate] = useState<string>('');
+    const [showPlaceholder, setShowPlaceholder] = useState<boolean>(true);*/
+    const navigate = useNavigate();
+
+    // Enable the save button only if all required fields are filled
+    const isSaveEnabled = firstName !== '' && lastName !== '' && dob !== '' && joinedDate !== '';
+
+    const handleFirstNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFirstName(event.target.value);
+    };
+
+    const handleLastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLastName(event.target.value);
+    };
+
+    const handleDobChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDob(event.target.value);
+        /*setSelectedDate(selectedDate);
+        setShowPlaceholder(selectedDate === '');*/ // Show placeholder if no date is selected
+    };
 
     const handleGenderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setGender(event.target.value);
     };
 
+    const handleJoinedDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setJoinedDate(event.target.value);
+        /*setSelectedDate(selectedDate);
+        setShowPlaceholder(selectedDate === '');*/ // Show placeholder if no date is selected
+    };
+
+    const handleTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setType(event.target.value);
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        // Add your form submission logic here
+        navigate('/users');
+    };
+
+    const handleCancel = () => {
+        navigate('/users');
+    };
+
     return (
         <div className="form-container">
             <label className="form-title">Create New User</label>
-            <form>
-                <label className="form-field">
-                    First Name <input type="text" />
-                </label>
-                <label className="form-field">
-                    Last Name <input type="text" />
-                </label>
-                <label className="form-field">
-                    Date of Birth <input type="date" />
-                </label>
-                <label className="form-label-gender">Gender</label>
-                <label className="form-radio">
-                    <input
-                        type="radio"
-                        name="gender"
-                        value="Female"
-                        checked={gender === 'Female'}
-                        onChange={handleGenderChange}
-                    />
-                    Female
-                </label>
-                <label className="form-radio">
-                    <input
-                        type="radio"
-                        name="gender"
-                        value="Male"
-                        checked={gender === 'Male'}
-                        onChange={handleGenderChange}
-                    />
-                    Male
-                </label>
-                <label className="form-field">
-                    Joined Date <input type="date" />
-                </label>
-                <label className="form-field">
-                    Type
-                    <select>
+            <form onSubmit={handleSubmit}>
+                <div className="form-field">
+                    <label>First Name</label>
+                    <input type="text" value={firstName} onChange={handleFirstNameChange} />
+                </div>
+                <div className="form-field">
+                    <label>Last Name</label>
+                    <input type="text" value={lastName} onChange={handleLastNameChange} />
+                </div>
+                <div className="form-field">
+                    <label>Date of Birth</label>
+                    <input type="date" value={dob} onChange={handleDobChange} /*style={{
+                        color: showPlaceholder ? 'initial' : 'transparent',
+                        display: showPlaceholder ? 'initial' : 'none'
+                    }}*/ />
+                </div>
+                <div className="form-field gender-field">
+                    <label>Gender</label>
+                    <div className="gender-options">
+                        <div className="gender-option">
+                            <input
+                                type="radio"
+                                name="gender"
+                                value="Female"
+                                checked={gender === 'Female'}
+                                onChange={handleGenderChange}
+                            />
+                            Female
+                        </div>
+                        <div className="gender-option">
+                            <input
+                                type="radio"
+                                name="gender"
+                                value="Male"
+                                checked={gender === 'Male'}
+                                onChange={handleGenderChange}
+                            />
+                            Male
+                        </div>
+                    </div>
+                </div>
+                <div className="form-field">
+                    <label>Joined Date</label>
+                    <input type="date" value={joinedDate} onChange={handleJoinedDateChange} /*style={{
+                        color: showPlaceholder ? 'initial' : 'transparent',
+                        display: showPlaceholder ? 'initial' : 'none'
+                    }}*/ />
+                </div>
+                <div className="form-field">
+                    <label>Type</label>
+                    <select value={type} onChange={handleTypeChange}>
                         <option>Staff</option>
                         <option>Admin</option>
                     </select>
-                </label>
+                </div>
                 <div className="form-buttons">
-                    <button type="submit" className="save-button">Save</button>
-                    <button type="button" className="cancel-button">Cancel</button>
+                    <button type="submit" className="save-button" disabled={!isSaveEnabled}>
+                        Save
+                    </button>
+                    <button type="button" className="cancel-button" onClick={handleCancel}>
+                        Cancel
+                    </button>
                 </div>
             </form>
         </div>
     );
-}
+};
 
 export default CreateUserPage;
