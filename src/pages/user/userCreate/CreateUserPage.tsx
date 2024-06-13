86f954c5bb6e9d377c3b96a297e62ded22440616
NhatMinh666 <nhatminh291297@gmail.com>
2024-06-13 13:07:47 +0700

    Update CreateUserPage.tsx
    Added custom form folder in components
    Customized form components with MUI

--- src/pages/user/userCreate/CreateUserPage.tsx
+++ src/pages/user/userCreate/CreateUserPage.tsx
@@ -1,53 +1,132 @@
-import { FC } from "react";
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './CreateUserForm.css';
+import {
+    Box,
+    Button,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Radio,
+    RadioGroup,
+    Select,
+    MenuItem,
+    TextField,
+    Typography,
+    InputLabel,
+} from '@mui/material';
+import { styled } from '@mui/system';
 
-const CreateUserPage: FC = () => {
+// Styled Components
+
+const FormContainer = styled(Box)(({ theme }) => ({
+    background: '#fff',
+    padding: '20px',
+    width: '450px',
+    textAlign: 'left',
+    borderRadius: '8px',
+    boxShadow: theme.shadows[2],
+    margin: '0 auto',
+    marginTop: theme.spacing(4),
+}));
+
+const FormTitle = styled(Typography)(({ theme }) => ({
+    fontWeight: 'bold',
+    color: 'red',
+    fontSize: '1.2em',
+    textAlign: 'center',
+    marginBottom: '20px',
+}));
+
+const FormField = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: '15px',
+    flexDirection: 'column',
+    [theme.breakpoints.up('sm')]: {
+        flexDirection: 'row',
+    },
+}));
+
+const FormLabelStyled = styled(InputLabel)(({ theme }) => ({
+    minWidth: '150px',
+    whiteSpace: 'nowrap',
+    textAlign: 'right',
+    paddingRight: '10px',
+    [theme.breakpoints.down('xs')]: {
+        textAlign: 'left',
+        paddingRight: '0',
+    },
+}));
+
+const TextFieldStyled = styled(TextField)(({ theme }) => ({
+    flex: 1,
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+    boxSizing: 'border-box',
+    marginLeft: '10px',
+    fontFamily: 'inherit',
+    [theme.breakpoints.down('xs')]: {
+        marginLeft: '0',
+        marginTop: '10px',
+    },
+}));
+
+const GenderOption = styled(FormControlLabel)(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: '15px',
+    '& > .MuiRadio-root': {
+        marginRight: '5px',
+    },
+}));
+
+const FormButtons = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '20px',
+    gap: theme.spacing(2),
+}));
+
+const SaveButton = styled(Button)(({ theme }) => ({
+    padding: '10px 20px',
+    margin: '5px',
+    borderRadius: '5px',
+    fontSize: '16px',
+    backgroundColor: 'red',
+    color: 'white',
+    '&:hover': {
+        backgroundColor: theme.palette.error.dark,
+    },
+}));
+
+const CancelButton = styled(Button)(({ theme }) => ({
+    padding: '10px 20px',
+    margin: '5px',
+    borderRadius: '5px',
+    fontSize: '16px',
+    border: '1px solid #ccc',
+    backgroundColor: 'white',
+    color: 'grey',
+    '&:hover': {
+        backgroundColor: theme.palette.action.hover,
+    },
+}));
+
+const CreateUserPage: React.FC = () => {
     const [firstName, setFirstName] = useState<string>('');
     const [lastName, setLastName] = useState<string>('');
     const [dob, setDob] = useState<string>('');
     const [gender, setGender] = useState<string>('Female');
     const [joinedDate, setJoinedDate] = useState<string>('');
     const [type, setType] = useState<string>('Staff');
-    /*const [selectedDate, setSelectedDate] = useState<string>('');
-    const [showPlaceholder, setShowPlaceholder] = useState<boolean>(true);*/
+
     const navigate = useNavigate();
 
-    // Enable the save button only if all required fields are filled
-    const isSaveEnabled = firstName !== '' && lastName !== '' && dob !== '' && joinedDate !== '';
-
-    const handleFirstNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFirstName(event.target.value);
-    };
-
-    const handleLastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setLastName(event.target.value);
-    };
-
-    const handleDobChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setDob(event.target.value);
-        /*setSelectedDate(selectedDate);
-        setShowPlaceholder(selectedDate === '');*/ // Show placeholder if no date is selected
-    };
-
-    const handleGenderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setGender(event.target.value);
-    };
-
-    const handleJoinedDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setJoinedDate(event.target.value);
-        /*setSelectedDate(selectedDate);
-        setShowPlaceholder(selectedDate === '');*/ // Show placeholder if no date is selected
-    };
-
-    const handleTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setType(event.target.value);
-    };
+    const isSaveEnabled = firstName && lastName && dob && joinedDate;
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        // Add your form submission logic here
+        // Add form submission logic here
         navigate('/users');
     };
 
@@ -56,73 +135,102 @@ const CreateUserPage: FC = () => {
     };
 
     return (
-        <div className="form-container">
-            <label className="form-title">Create New User</label>
+        <FormContainer>
+            <FormTitle>Create New User</FormTitle>
             <form onSubmit={handleSubmit}>
-                <div className="form-field">
-                    <label>First Name</label>
-                    <input type="text" value={firstName} onChange={handleFirstNameChange} />
-                </div>
-                <div className="form-field">
-                    <label>Last Name</label>
-                    <input type="text" value={lastName} onChange={handleLastNameChange} />
-                </div>
-                <div className="form-field">
-                    <label>Date of Birth</label>
-                    <input type="date" value={dob} onChange={handleDobChange} /*style={{
-                        color: showPlaceholder ? 'initial' : 'transparent',
-                        display: showPlaceholder ? 'initial' : 'none'
-                    }}*/ />
-                </div>
-                <div className="form-field gender-field">
-                    <label>Gender</label>
-                    <div className="gender-options">
-                        <div className="gender-option">
-                            <input
-                                type="radio"
-                                name="gender"
+                <FormField>
+                    <FormLabelStyled htmlFor="firstName">First Name</FormLabelStyled>
+                    <TextFieldStyled
+                        id="firstName"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        variant="outlined"
+                        fullWidth
+                        margin="dense"
+                    />
+                </FormField>
+                <FormField>
+                    <FormLabelStyled htmlFor="lastName">Last Name</FormLabelStyled>
+                    <TextFieldStyled
+                        id="lastName"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                        variant="outlined"
+                        fullWidth
+                        margin="dense"
+                    />
+                </FormField>
+                <FormField>
+                    <FormLabelStyled htmlFor="dob">Date of Birth</FormLabelStyled>
+                    <TextFieldStyled
+                        id="dob"
+                        type="date"
+                        value={dob}
+                        onChange={(e) => setDob(e.target.value)}
+                        variant="outlined"
+                        fullWidth
+                        margin="dense"
+                        InputLabelProps={{ shrink: true }}
+                    />
+                </FormField>
+                <FormField>
+                        <FormLabelStyled htmlFor="gender">Gender</FormLabelStyled>
+                        <RadioGroup
+                            row
+                            name="gender"
+                            value={gender}
+                            onChange={(e) => setGender(e.target.value)}
+                        >
+                            <GenderOption
                                 value="Female"
-                                checked={gender === 'Female'}
-                                onChange={handleGenderChange}
+                                control={<Radio />}
+                                label="Female"
                             />
-                            Female
-                        </div>
-                        <div className="gender-option">
-                            <input
-                                type="radio"
-                                name="gender"
+                            <GenderOption
                                 value="Male"
-                                checked={gender === 'Male'}
-                                onChange={handleGenderChange}
+                                control={<Radio />}
+                                label="Male"
                             />
-                            Male
-                        </div>
-                    </div>
-                </div>
-                <div className="form-field">
-                    <label>Joined Date</label>
-                    <input type="date" value={joinedDate} onChange={handleJoinedDateChange} /*style={{
-                        color: showPlaceholder ? 'initial' : 'transparent',
-                        display: showPlaceholder ? 'initial' : 'none'
-                    }}*/ />
-                </div>
-                <div className="form-field">
-                    <label>Type</label>
-                    <select value={type} onChange={handleTypeChange}>
-                        <option>Staff</option>
-                        <option>Admin</option>
-                    </select>
-                </div>
-                <div className="form-buttons">
-                    <button type="submit" className="save-button" disabled={!isSaveEnabled}>
+                        </RadioGroup>
+                </FormField>
+                <FormField>
+                    <FormLabelStyled htmlFor="joinedDate">Joined Date</FormLabelStyled>
+                    <TextFieldStyled
+                        id="joinedDate"
+                        type="date"
+                        value={joinedDate}
+                        onChange={(e) => setJoinedDate(e.target.value)}
+                        variant="outlined"
+                        fullWidth
+                        margin="dense"
+                        InputLabelProps={{ shrink: true }}
+                    />
+                </FormField>
+                <FormField>
+                    <FormLabelStyled htmlFor="type">Type</FormLabelStyled>
+                    <FormControl fullWidth margin="dense">
+                        <Select
+                            id="type"
+                            value={type}
+                            onChange={(e) => setType(e.target.value as string)}
+                            displayEmpty
+                            variant="outlined"
+                        >
+                            <MenuItem value="Staff">Staff</MenuItem>
+                            <MenuItem value="Admin">Admin</MenuItem>
+                        </Select>
+                    </FormControl>
+                </FormField>
+                <FormButtons>
+                    <SaveButton type="submit" variant="contained" disabled={!isSaveEnabled}>
                         Save
-                    </button>
-                    <button type="button" className="cancel-button" onClick={handleCancel}>
+                    </SaveButton>
+                    <CancelButton type="button" variant="outlined" onClick={handleCancel}>
                         Cancel
-                    </button>
-                </div>
+                    </CancelButton>
+                </FormButtons>
             </form>
-        </div>
+        </FormContainer>
     );
 };
 
